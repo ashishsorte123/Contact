47f3d4af1a389945e2cb02c87271017311e6c30c
ashishsorte123 <88495557+ashishsorte123@users.noreply.github.com>
2021-12-20 14:36:01 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import 'react-native-gesture-handler';
+import React from 'react';
+import AppNavContainer from './src/navigations';
+
+const App = () => {
+  return (
+    <AppNavContainer> 
+      
+    </AppNavContainer>    
+  );
+};
+
+export default App;

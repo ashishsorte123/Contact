f423c06d34cabd3f2264812152b7a7364c5db7c4
ashishsorte123 <88495557+ashishsorte123@users.noreply.github.com>
2021-12-21 19:47:59 +0530

    first commit

---
+++ src/components/Signup/index.js
@@ -0,0 +1,132 @@
+import {useNavigation} from '@react-navigation/native';
+import React, {useState} from 'react';
+import {Image, Text, View} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import Container from '../common/Container';
+import CustomButton from '../common/CustomButton';
+import Input from '../common/Input';
+import {LOGIN} from '../../constants/routeNames';
+import Message from '../common/Message';
+import styles from './styles';
+
+const RegisterComponent = ({
+  error,
+  form,
+  justSignedUp,
+  onChange,
+  loading,
+  onSubmit,
+}) => {
+  const {navigate} = useNavigation();
+  const [isSecureEntry, setIsSecureEntry] = useState(true);
+  return (
+    <Container>
+      <Image
+        height={70}
+        width={70}
+        source={require('../../assets/images/logo.png')}
+        style={styles.logoImage}
+      />
+
+      <View>
+        <Text style={styles.title}>Welcome to Contacts</Text>
+        <Text style={styles.subTitle}>Create a free account</Text>
+
+        <View style={styles.form}>
+          {justSignedUp && (
+            <Message
+              onDismiss={() => {}}
+              success
+              message="Account created successfully"
+            />
+          )}
+          {error && !error.error && (
+            <Message
+              onDismiss={() => {}}
+              danger
+              message="invalid credentials"
+            />
+          )}
+
+          {error?.error && <Message danger onDismiss message={error?.error} />}
+
+          <Input
+            label="Username"
+            iconPosition="right"
+            placeholder="Enter Username"
+            // value={form.userName || null}
+            // onChangeText={value => {
+            //   onChange({name: 'userName', value});
+            // }}
+            // error={'This field is required'}
+          />
+          <Input
+            label="First Name"
+            iconPosition="right"
+            placeholder="Enter First name"
+            // value={form.userName || null}
+            // onChangeText={value => {
+            //   onChange({name: 'userName', value});
+            // }}
+          />
+          <Input
+            label="Last Name"
+            iconPosition="right"
+            placeholder="Enter Last name"
+            // value={form.userName || null}
+            // onChangeText={value => {
+            //   onChange({name: 'userName', value});
+            // }}
+          />
+          <Input
+            label="Email"
+            iconPosition="right"
+            placeholder="Enter Email"
+            // value={form.userName || null}
+            // onChangeText={value => {
+            //   onChange({name: 'userName', value});
+            // }}
+          />
+
+          <Input
+            label="Password"
+            placeholder="Enter Password"
+            secureTextEntry={isSecureEntry}
+            icon={
+              <TouchableOpacity
+                onPress={() => {
+                  setIsSecureEntry(prev => !prev);
+                }}>
+                <Text>{isSecureEntry ? 'Show' : 'Hide'}</Text>
+              </TouchableOpacity>
+            }
+            iconPosition="right"
+            // onChangeText={value => {
+            //   onChange({name: 'password', value});
+            // }}
+          />
+
+          <CustomButton
+            disabled={loading}
+            onPress={onSubmit}
+            loading={loading}
+            primary
+            title="Submit"
+          />
+
+          <View style={styles.createSection}>
+            <Text style={styles.infoText}>Already have an account ?</Text>
+            <TouchableOpacity
+              onPress={() => {
+                navigate(LOGIN);
+              }}>
+              <Text style={styles.linkBtn}>Login</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </Container>
+  );
+};
+
+export default RegisterComponent;

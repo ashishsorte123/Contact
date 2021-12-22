a4ab8240f6901d639c7ba2bc0f2083534e00e09f
ashishsorte123 <88495557+ashishsorte123@users.noreply.github.com>
2021-12-23 00:11:48 +0530

    first commit

--- babel.config.js
+++ babel.config.js
@@ -2,20 +2,12 @@ module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     [
-      'module-resolver',
-      {
-        extensions: ['.tsx', '.ts', '.js', '.json'],
-      },
-    ],
-    'react-native-reanimated/plugin',
-  ],
-  plugins: [
-    [
       'module:react-native-dotenv',
       {
         moduleName: '@env',
         path: '.env',
       },
     ],
+    ['react-native-reanimated/plugin'],
   ],
 };

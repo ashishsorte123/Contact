aa0e67ce152917c4957d05291a2f10d8cb2b3582
ashishsorte123 <88495557+ashishsorte123@users.noreply.github.com>
2021-12-27 23:15:39 +0530

    first commit

--- src/screens/CreateContact/index.js
+++ src/screens/CreateContact/index.js
@@ -3,10 +3,8 @@ import CreateContactComponent from '../../components/CreateContactComponent';
 import createContact from '../../context/actions/contacts/createContact';
 import {GlobalContext} from '../../context/Provider';
 import {useNavigation, useRoute} from '@react-navigation/native';
-import {CONTACT_DETAIL, CONTACT_LIST} from '../../constants/routeNames';
-// import uploadImage from '../../helpers/uploadImage';
-// import countryCodes from '../../utils/countryCodes';
-// import editContact from '../../context/actions/contacts/editContact';
+import {CONTACT_LIST} from '../../constants/routeNames';
+import uploadImage from '../../helpers/uploadImage';
 
 const CreateContact = () => {
   const {
@@ -20,94 +18,29 @@ const CreateContact = () => {
   const [form, setForm] = useState({});
   const [uploading, setIsUploading] = useState(false);
   const {navigate, setOptions} = useNavigation();
-  const {params} = useRoute();
-
   const [localFile, setLocalFile] = useState(null);
-
-  useEffect(() => {
-    if (params?.contact) {
-      setOptions({title: 'Update contact'});
-      const {
-        first_name: firstName,
-        phone_number: phoneNumber,
-        last_name: lastName,
-        is_favorite: isFavorite,
-        country_code: countryCode,
-      } = params?.contact;
-
-      setForm(prev => {
-        return {
-          ...prev,
-          firstName,
-          isFavorite,
-          phoneNumber,
-          lastName,
-          phoneCode: countryCode,
-        };
-      });
-
-      const country = countryCodes.find(item => {
-        return item.value.replace('+', '') === countryCode;
-      });
-
-      if (country) {
-        setForm(prev => {
-          return {
-            ...prev,
-            countryCode: country.key.toUpperCase(),
-          };
-        });
-      }
-
-      if (params?.contact?.contact_picture) {
-        setLocalFile(params?.contact.contact_picture);
-      }
-    }
-  }, []);
 
   const onChangeText = ({name, value}) => {
     setForm({...form, [name]: value});
   };
 
   const onSubmit = () => {
-    if (params?.contact) {
-      if (localFile?.size) {
-        setIsUploading(true);
-        uploadImage(localFile)(url => {
-          setIsUploading(false);
-          editContact(
-            {...form, contactPicture: url},
-            params?.contact.id,
-          )(contactsDispatch)(item => {
-            navigate(CONTACT_DETAIL, {item});
-          });
-        })(err => {
-          setIsUploading(false);
-        });
-      } else {
-        editContact(form, params?.contact.id)(contactsDispatch)(item => {
-          navigate(CONTACT_DETAIL, {item});
-        });
-      }
-    } else {
-      if (localFile?.size) {
-        setIsUploading(true);
-        uploadImage(localFile)(url => {
-          setIsUploading(false);
-          createContact({...form, contactPicture: url})(contactsDispatch)(
-            () => {
-              navigate(CONTACT_LIST);
-            },
-          );
-        })(err => {
-          setIsUploading(false);
-        });
-      } else {
-        createContact(form)(contactsDispatch)(() => {
+    console.log('localFile ;>> ', localFile);
+    if (localFile?.size) {
+      setIsUploading(true);
+      uploadImage(localFile)(url => {
+        setIsUploading(false);
+        createContact({...form, contactPicture: url})(contactsDispatch)(() => {
           navigate(CONTACT_LIST);
         });
-      }
+      })(error => {
+        console.log('error :>> ', error);
+        setIsUploading(false);
+      });
     }
+    createContact(form)(contactsDispatch)(() => {
+      navigate(CONTACT_LIST);
+    });
   };
 
   const closeSheet = () => {

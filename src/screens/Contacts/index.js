0b927fc7d278931352e4fa36ec1159fb4407a8f2
ashishsorte123 <88495557+ashishsorte123@users.noreply.github.com>
2021-12-28 15:38:06 +0530

    first commit

--- src/screens/Contacts/index.js
+++ src/screens/Contacts/index.js
@@ -1,16 +1,19 @@
 import {useNavigation, useFocusEffect} from '@react-navigation/native';
-import React, {useState, useContext, useEffect} from 'react';
+import React, {useState, useContext, useEffect, useRef} from 'react';
 import {TouchableOpacity} from 'react-native';
 import Icon from '../../components/common/Icon';
 import ContactsComponent from '../../components/ContactsComponent';
 import getContacts from '../../context/actions/contacts/getContacts';
 import {GlobalContext} from '../../context/Provider';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import {navigate} from '../../navigations/SideMenu/RootNavigator';
+import {CONTACT_DETAIL} from '../../constants/routeNames';
 
 const Contacts = ({navigation}) => {
   const [sortBy, setSortBy] = React.useState(null);
   const {setOptions, toggleDrawer} = useNavigation();
   const [modalVisible, setModalVisible] = useState(false);
+  const contactsRef = useRef(false);
   const {
     contactsDispatch,
     contactsState: {
@@ -38,6 +41,25 @@ const Contacts = ({navigation}) => {
       return () => {};
     }, []),
   );
+
+  useEffect(() => {
+    contactsRef.current = true;
+    return () => {
+      contactsRef.current = false;
+    };
+  }, []);
+
+  useEffect(() => {
+    const prev = contactsRef.current;
+    contactsRef.current = data;
+    const newList = contactsRef.current;
+    if (newList.length - prev.length === 1) {
+      const newContact = newList.find(
+        item => !prev.map(i => i.id).includes(item.id),
+      );
+      navigate(CONTACT_DETAIL, {item: newContact});
+    }
+  }, [data.length]);
 
   React.useEffect(() => {
     setOptions({
